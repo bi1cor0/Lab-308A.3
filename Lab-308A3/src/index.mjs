f034497d593eebbc63447bda864552b8f68f8fc4
Bill C. <106033471+bi1cor0@users.noreply.github.com>
2024-05-30 14:35:01 -0700

    got help with await and async. edited code to retrieve data and transform it into an object.
    Was having trouble with understanding await and async. But managed to get some more perspective on this.

--- Lab-308A3/src/index.mjs
+++ Lab-308A3/src/index.mjs
@@ -1,12 +1,29 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.mjs";
 
-function getUserData(id) {
-  const dbs = {
-    db1: db1(id),
-    db2: db2(id),
-    db3: db3(id)
+async function getUserData(id) {
+  const dbs = { //object of functions. in order to put the value of the function in an object key, place only the name of the functiton.
+    db1: db1,
+    db2: db2,
+    db3: db3
   };
+  const returnedValue = await central(id);
+  const returnedData = await dbs[returnedValue](id);
+  const returnedPII = await vault(id);
+
+  const results = {
+    id: id,
+    name: returnedPII.name,
+    username: returnedData.username,
+    email: returnedPII.email,
+    address: returnedPII.address,
+    phone: returnedPII.phone,
+    website: returnedData.website,
+    company: returnedData.company
+  }
+
+  return results;
 }
 
-console.log(getUserData(5));
+const info = await getUserData(6);
+console.log(info)
